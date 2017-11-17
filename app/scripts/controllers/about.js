50ed5636b16826b5a3eb33e67a4a313009991ff1
Guillermo <guillermo.bascunana@quantion.com>
2017-11-17 10:14:14 +0100

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name betoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the betoApp
+ */
+angular.module('betoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name betoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the betoApp
+ */
+angular.module('betoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
